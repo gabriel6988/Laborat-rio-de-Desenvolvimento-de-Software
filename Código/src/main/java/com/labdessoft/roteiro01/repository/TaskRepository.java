faa2e91a1561199b476cb0aa3471e99d39e8cd30
Gabriel Alejandro Figueiro Galindo <83712515+gabriel6988@users.noreply.github.com>
2024-04-09 22:48:16 -0300

    Delete Código directory

--- Código/src/main/java/com/labdessoft/roteiro01/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.labdessoft.roteiro01.repository;
-
-import com.labdessoft.roteiro01.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long>{}
