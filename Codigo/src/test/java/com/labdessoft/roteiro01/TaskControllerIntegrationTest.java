b6843e1598109fdc369773b68aac02562a11f306
Gabriel Alejandro Figueiro Galindo <83712515+gabriel6988@users.noreply.github.com>
2024-05-12 18:39:27 -0300

    Add files via upload

--- Codigo/src/test/java/com/labdessoft/roteiro01/TaskControllerIntegrationTest.java
+++ Codigo/src/test/java/com/labdessoft/roteiro01/TaskControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.labdessoft.roteiro01;
 
 import com.labdessoft.roteiro01.entity.Task;
+import com.labdessoft.roteiro01.entity.TaskData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,12 +9,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,15 +30,11 @@ public class TaskControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @LocalServerPort
-    private int port;
-
     private String baseUrl;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        baseUrl = "http://localhost:" + port + "/tasks";
+        baseUrl = "http://localhost:" + restTemplate.getRootUri().split(":")[2] + "/tasks";
     }
 
     @Test
@@ -49,22 +49,46 @@ public class TaskControllerIntegrationTest {
         Task createdTask = response.getBody();
         assertThat(createdTask).isNotNull();
         assertThat(createdTask.getDescription()).isEqualTo("Nova Tarefa");
+        /*TaskData taskData;
+        taskData = new TaskData();
+        taskData.setId(1L);
+        taskData.setDescription("Exemplo 1");
+        taskData.setCompleted(false);
+        taskData.setPriority(Task.Priority.ALTA);
+        taskData.setDueDate(LocalDate.now().plusDays(7));
+
+        ResponseEntity<TaskData> response = restTemplate.postForEntity(baseUrl, taskData, TaskData.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        TaskData createdTask = response.getBody();
+        assertThat(createdTask).isNotNull();
+        assertThat(createdTask.getDescription()).isEqualTo("Nova Tarefa");*/
     }
 
     @Test
     public void testGetAllTasks() {
-        ResponseEntity<List> response = restTemplate.getForEntity(baseUrl, List.class);
+        ResponseEntity<List<Task>> response = restTemplate.exchange(
+                "/tasks",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Task>>() {});
+
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         List<Task> tasks = response.getBody();
         assertThat(tasks).isNotNull();
         assertThat(tasks.size()).isGreaterThan(0);
+
+        MediaType contentType = response.getHeaders().getContentType();
+        assertThat(contentType).isNotNull();
+        assertThat(contentType.toString()).isEqualTo("application/json");
     }
 
     @Test
     public void testCompleteTask() {
         long taskId = 1L;
-        ResponseEntity<Void> response = restTemplate.patchForObject(baseUrl + "/" + taskId + "/completed", null, ResponseEntity.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        ResponseEntity<?> responseEntity = restTemplate.patchForObject(
+                baseUrl + "/" + taskId + "/completed", null,
+                ResponseEntity.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 
     @Test
@@ -93,7 +117,6 @@ public class TaskControllerIntegrationTest {
 
     @Test
     public void testDeleteCompletedTasks() {
-        // Crie e conclua algumas tarefas;
         Task task1 = new Task();
         task1.setDescription("Task 1");
         task1.setCompleted(true);
@@ -102,11 +125,12 @@ public class TaskControllerIntegrationTest {
         task2.setDescription("Task 2");
         task2.setCompleted(true);
 
-        // Chama o endpoint deleteCompletedTasks;
         restTemplate.delete(baseUrl + "/completed");
 
-        // Recuperar todas as tarefas e verificar se as tarefas concluídas foram excluídas;
-        ResponseEntity<List> response = restTemplate.getForEntity(baseUrl, List.class);
+        ResponseEntity<List<Task>> response = restTemplate.exchange(
+                baseUrl, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Task>>() {});
+
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         List<Task> tasks = response.getBody();
         assertThat(tasks).isNotNull();
