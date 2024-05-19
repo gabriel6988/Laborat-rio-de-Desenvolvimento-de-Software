ea331000f8356f257632f0098c8d21fbc33ecb21
Gabriel <gabrielfgalindo@gmail.com>
2024-05-19 14:05:24 -0300

    Testes

--- Codigo/src/test/java/com/labdessoft/roteiro01/TaskMock.java
+++ Codigo/src/test/java/com/labdessoft/roteiro01/TaskMock.java
@@ -1,14 +1,14 @@
-/*package com.labdessoft.roteiro01;
+package com.labdessoft.roteiro01;
 
 import com.labdessoft.roteiro01.entity.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskMock {
     public static Page<Task> createTasks() {
-
         List<Task> taskList = new ArrayList<>();
         Task task1 = new Task();
         task1.setId(1L);
@@ -20,7 +20,12 @@ public class TaskMock {
 
         taskList.add(task1);
         taskList.add(task2);
-        Page<Task> pagedResponse = new PageImpl(taskList);
+
+        // Criando um PageRequest com índice de página e tamanho de página baseado em zero;
+        PageRequest pageRequest = PageRequest.of(0, taskList.size());
+
+        // Criando um PageImpl usando a lista de tarefas e solicitação de página;
+        Page<Task> pagedResponse = new PageImpl<>(taskList, pageRequest, taskList.size());
         return pagedResponse;
     }
-}*/
+}
